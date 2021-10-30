93be3da76777c8684b5f2b219114da40a7947950
VedantNakrani <vedant2301nakrani@gmail.com>
2021-10-30 21:48:42 +0530

    add income

---
+++ controllerIncomeData.php
@@ -0,0 +1,70 @@
+<?php 
+require "connection.php";
+require 'env.php';
+$email = $_SESSION["email"];
+$errors = array();
+
+//if user adds new income
+if(isset($_POST['add-income'])){
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $category = mysqli_real_escape_string($con, $_POST['category']);
+    $new_category = mysqli_real_escape_string($con, $_POST['new-category']);
+    $amount = mysqli_real_escape_string($con, $_POST['amount']);
+    $date = mysqli_real_escape_string($con, $_POST['date']);
+    if(!$name || !$category || !$amount || !$date){
+        $errors['empty-field'] = "Fields must not ne empty!";
+    }
+    else {
+        $id = 0;
+        $res = mysqli_query($con, "SELECT MAX(id_) AS id FROM income;");
+        if(mysqli_num_rows($res) > 0){
+            $id_data = mysqli_fetch_array($res);
+            $id = $id_data["id"] + 1;
+        }
+        if($new_category == ""){
+            $add_income = "INSERT INTO income VALUES ('$email', $id, '$name', $amount, 0, '$category', '$date');";
+        }
+        else {
+            $add_income = "INSERT INTO income VALUES ('$email', $id, '$name', $amount, 0, '$new_category', '$date');";
+        }
+        $run_query = mysqli_query($con, $add_income);
+        if($run_query){
+            $info = "Income added successfully!";
+            $_SESSION['info'] = $info;
+            header('Location: income.php');
+        }else{
+            $errors['db-error'] = "Failed to add income!";
+        }
+    }
+}
+
+//if user adds new income
+if(isset($_POST['delete_income'])){
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $category = mysqli_real_escape_string($con, $_POST['category']);
+    $new_category = mysqli_real_escape_string($con, $_POST['new-category']);
+    $amount = mysqli_real_escape_string($con, $_POST['amount']);
+    $date = mysqli_real_escape_string($con, $_POST['date']);
+    if(!$name || !$category || !$amount || !$date){
+        $errors['empty-field'] = "Fields must not ne empty!";
+    }
+    else {
+        $id = 5;
+        if($new_category == ""){
+            $add_income = "INSERT INTO income VALUES ('$email', $id, '$name', $amount, 0, '$category', '$date');";
+        }
+        else {
+            $add_income = "INSERT INTO income VALUES ('$email', $id, '$name', $amount, 0, '$new_category', '$date');";
+        }
+        $run_query = mysqli_query($con, $add_income);
+        if($run_query){
+            $info = "Income added successfully!";
+            $_SESSION['info'] = $info;
+            header('Location: income.php');
+        }else{
+            $errors['db-error'] = "Failed to add income!";
+        }
+    }
+}
+
+?>
